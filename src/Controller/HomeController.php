0c65afebf68da542dcc057a74879ecedb9ad811e
CharlesLLM <charlesllm@orange.fr>
2022-11-30 10:18:33 +0100

    Ajout home

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route(path: "/", name: "home")]
+    public function home(Request $request): Response
+    {
+        return $this->render('home/home.html.twig', [
+            'name' => 'Charles',
+        ]);
+    }
+}
